cedb1f309be72e7758706c5376ead33a2e2a4a79
ing-chiba <chiba@in-g.jp>
2025-09-06 15:26:33 +0900

    設定変更

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,16 @@ const nextConfig: NextConfig = {
   experimental: {
     optimizePackageImports: ["@chakra-ui/react"],
   },
+  // TODO: 本番反映のタイミングでこの設定を削除
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
