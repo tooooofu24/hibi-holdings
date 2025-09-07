7dab59f2fdcc6ee867dc7899162c6a1d0f691f26
ing-chiba <chiba@in-g.jp>
2025-09-08 07:59:08 +0900

    修正

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,18 +1,18 @@
 import { VStack } from "@chakra-ui/react";
-import { PageLayout } from "../components/PageLayout";
 import { PageHeader } from "../components/PageHeader";
+import { PageLayout } from "../components/PageLayout";
 import { ContactForm } from "./components/ContactForm";
 
 export default function ContactPage() {
   return (
     <PageLayout>
       <VStack gap={{ base: "16", md: "24" }} align="stretch">
-            <PageHeader
-              title="お問い合わせ"
-              description="お問合せ・ご相談は下記のフォームにご記入下さい。内容を確認後、メールにてお返事させて頂きます。"
-            />
+        <PageHeader
+          title="お問い合わせ"
+          description="お問合せ・ご相談は下記のフォームにご記入下さい。内容を確認後、メールにてお返事させて頂きます。"
+        />
 
-            <ContactForm />
+        <ContactForm />
       </VStack>
     </PageLayout>
   );
